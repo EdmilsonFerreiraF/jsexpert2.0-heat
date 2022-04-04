65894b8ddbc9f52d4652bdf78f259203f9e4546e
EdmilsonFerreiraF <Edmilsonf.o3@gmail.com>
2022-04-04 19:15:12 -0300

    🚀 :rocket: [Add] Employee grosspay test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@
 // }).#name)
 
 const assert = require('assert')
+const Util = require('../util')
 const Employee = require('./employee')
 
 const GENDER = {
@@ -37,5 +38,5 @@ const GENDER = {
     assert.deepStrictEqual(employee.name, "Mr. Edmilson Ferreira")
     assert.deepStrictEqual(employee.age, undefined)
     assert.deepStrictEqual(employee.gender, undefined)
-    assert.deepStrictEqual(employee.grossPay, 0)
+    assert.deepStrictEqual(employee.grossPay, Util.formatCurrency(5000.40))
 }
